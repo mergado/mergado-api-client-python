efebadedf265d10243bdf7d70295e570af681846
Pavel Dedík <dedikx@gmail.com>
2017-04-21 13:26:17 +0200

    Removed forgotten constants.

--- mergadoapiclient/__init__.py
+++ mergadoapiclient/__init__.py
@@ -3,10 +3,3 @@
 
 __version__ = '0.1.0'
 __authors__ = [u'Lukáš Ševčík', u'Peter Smatana', u'Pavel Dedík']
-
-
-USER_AGENT = 'Mergado-Api-Client-Python/version-{}'.format(__version__)
-
-TOKEN_URI = 'https://app.mergado.com/oauth2/token/'
-
-MERGADO_API_URI = 'https://app.mergado.com/api/'
