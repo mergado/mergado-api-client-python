dd468c150e6b038f8d92db6797a1e4e01a98ce02
Pavel Dedik <dedikx@gmail.com>
2017-12-12 16:10:18 +0100

    Update API URL in config.py

--- mergadoapiclient/config.py
+++ mergadoapiclient/config.py
@@ -8,6 +8,6 @@ USER_AGENT = 'Mergado-Api-Client-Python/version-{}'.format(ver)
 
 TOKEN_URI = 'https://app.mergado.com/oauth2/token/'
 
-MERGADO_API_URI = 'https://app.mergado.com/api/'
+MERGADO_API_URI = 'https://api.mergado.com/'
 
 TOKEN_STORAGE_CLASS = 'mergadoapiclient.storage.BaseTokenStorage'
