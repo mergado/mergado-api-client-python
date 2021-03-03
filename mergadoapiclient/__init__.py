a23df05ed6800881f5269d73731bf03de5f1740c
Matej Hamala <matej.hamala@mergado.com>
2021-03-03 14:25:37 +0100

    Bumb version

--- mergadoapiclient/__init__.py
+++ mergadoapiclient/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = '0.1.3'
-__authors__ = [u'Lukáš Ševčík', u'Peter Smatana', u'Pavel Dedík']
+__version__ = '0.1.4'
+__authors__ = ['Lukáš Ševčík', 'Peter Smatana', 'Pavel Dedík', 'Matěj Hamala']
