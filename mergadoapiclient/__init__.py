496640993ea14f478c495499007e728cfbd0dd01
kristiankralovic <kristian.kralovic@mergado.com>
2024-12-09 12:28:20 +0100

    Bumb version

--- mergadoapiclient/__init__.py
+++ mergadoapiclient/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __authors__ = ['Lukáš Ševčík', 'Peter Smatana', 'Pavel Dedík', 'Matěj Hamala',
-               'Alžbeta Mazurkovičová']
+               'Alžbeta Mazurkovičová', 'Kristián Královič']
